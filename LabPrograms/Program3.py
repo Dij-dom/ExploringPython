3f3b7fcdf6cbfea7df00d95991a40f344779360c
CodingPanda <dijin1906@gmail.com>
2023-08-30 12:50:19 +0530

    updated regex

--- LabPrograms/Program3.py
+++ LabPrograms/Program3.py
@@ -14,18 +14,22 @@ class Automate:
                         print(name)
                 except NameError:
                     print("Can't find the name with respect to the pattern entered")
+
             elif ch == 2:
                 report_dates = re.findall(r"Date of report: (.+)", content)
                 for date in report_dates:
                     print(date)
+
             elif ch ==3:
                 lab_technician_emails = re.findall(r"Email id of the lab technician: (\S+)", content)
                 for email in lab_technician_emails:
                     print(email)
+
             elif ch ==4:
                 doctor_names = re.findall(r"Name of the Doctor: (Dr\. .+)", content)
                 for doctor in doctor_names:
                     print(doctor)
+
             elif ch ==5:
                 try:
                     patient_ages = re.findall(r"Age of the patient: (\d+)", content)
@@ -37,15 +41,19 @@ class Automate:
                         print(age)
                 except NoPatientAgesFoundException as e:
                     print(e)
+
             elif ch ==6:
                 websites = re.findall(r"Website if any: (www\.\S+)?", content) # add all the white space chatracter after this
                 for website in websites:
                     print(website)
+
             elif ch ==7:
                 updated_data = re.sub(r"(www\.\S+)\.com", r"\1.in", content) #\1: This is a special reference to the first capturing group (the part in parentheses) in the original pattern. In this case, it refers to the captured website URL without the ".com" part. and add .in
                 print(updated_data)
+
             elif ch <1 or ch>9:
                 print("Enter a valid input")
+                
             else:
                 break
 
